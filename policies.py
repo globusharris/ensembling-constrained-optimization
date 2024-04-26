04208a26c8155b408584062d39938da782510fcb
Ira Globus-Harris <ira@iras-mbp.mynetworksettings.com>
2024-04-26 10:03:23 -0700

    ported variance constrained policy into experiment workflow

--- policies.py
+++ policies.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cvxpy as cp
+from sklearn.covariance import empirical_covariance
 
 class Policy:
 
@@ -29,7 +31,7 @@ class Simplex(Policy):
     def __init__(self, dim, model):
         Policy.__init__(self, dim, model)
         self.name = "simplex"
-        self.coordinate_values = [1]
+        self.coordinate_values = [1] 
         self.n_vals = len(self.coordinate_values)
     
     def run(self, xs):
@@ -49,5 +51,46 @@ class Simplex(Policy):
         allocation = np.zeros((len(preds), self.dim))
         max_coord = np.argmax(preds, axis=1)
         allocation[np.arange(len(preds)), max_coord] = np.ones(len(preds))
+        return allocation
+
+class VarianceConstrained(Policy):
+
+    def __init__(self, dim, model, gran, var_limit, ys):
+        Policy.__init__(self, dim, model)
+        self.name = "minimize-variance"
+        self.gran = gran
+        self.var_limit = var_limit
+        self.ys = ys
+        self.covariance = empirical_covariance(ys, assume_centered=False)
+        self.coordinate_values = np.arange(0,1,gran)
+        self.n_vals = len(self.coordinate_values)
+ 
+    
+    def run_given_preds(self, preds):
+        """
+        preds: array of predictions, where one row corresponds to a single vector of predictions.
+        return: allocation vector for each prediction.
+        The allocation vector is computed by constraining the problem to maximize the policy subject to
+        it summing to 1 across all coordinates and, if w is the policy vector, wCw^T <= alpha, where
+        C is the (empirical) covariance matrix of the true ys.
+        
+        It runs this optimization problem separately for each prediction vector that is input. 
+        """
+
+        ### Question: is there a way to run this in parallel for each of the predictions that is better?
+        # I feel like there must be. 
+
+        allocation = np.zeros((len(preds), self.dim))
+        for i in range(len(preds)):
+            x = cp.Variable(self.dim)
+            objective = cp.Maximize(x @ preds[i])
+            # for some reason, the last constraint isn't actually constraining things to be less than 1? 
+            constraints = [x<=1, x>=0, x @ self.covariance @ x.T <= self.var_limit, x @ np.ones(self.dim) <= 1]
+            prob = cp.Problem(objective, constraints)
+            obj = prob.solve()  
+            solution = x.value
+            tau = int(abs(np.log10(self.gran)))
+            rounded_solution = np.round(solution, tau)
+            allocation[i] = rounded_solution
         return allocation
 
