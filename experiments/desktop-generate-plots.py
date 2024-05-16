0794031cdf89582abddf3b7c67309c32ce1ca12a
vargpt <gupta1.varun@gmail.com>
2024-05-16 11:49:40 -0400

    Added final basic synthetic experiment results

--- experiments/desktop-generate-plots.py
+++ experiments/desktop-generate-plots.py
@@ -8,7 +8,7 @@ sys.path.append('experiment-pipeline')
 
 import analysisHelp
 
-experimentName='poly-label_gb_coord_variance'
+experimentName='linear-label_gb_coord_variance'
 
 bbPath = 'experiment-pipeline/debiased-models/desktop-experiments/{0}_BBModel.pkl'.format(experimentName)
 maxEnsemblePath = 'experiment-pipeline/debiased-models/desktop-experiments/{0}_MaxEnsemble.pkl'.format(experimentName)
--- experiments/experiment-pipeline/experiment-script.sh
+++ experiments/experiment-pipeline/experiment-script.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Run the data generation 
+# Run the data generation
 #python dataGeneration.py
 
-# # Run model generation 
+# # Run model generation
 #python modelSetup.py
 
-#python debiasingModels.py linear-label gb coord variance 600
+echo starting1 >> status.txt
+echo $(date) >> status.txt
+python debiasingModels.py linear-label gb coord variance 1000
 # python debiasingModels.py linear-label gb group variance 600
 # python debiasingModels.py linear-label gb all variance 600
-
-python debiasingModels.py linear-label lin coord variance 2
+echo starting2 >> status.txt
+echo $(date) >> status.txt
+python debiasingModels.py linear-label lin coord variance 1000
 # python debiasingModels.py linear-label lin coord variance 600
 # python debiasingModels.py linear-label lin coord variance 600
-
-#python debiasingModels.py poly-label gb coord variance 600
+echo starting3 >> status.txt
+echo $(date) >> status.txt
+python debiasingModels.py poly-label gb coord variance 1000
 # python debiasingModels.py poly-label gb group variance 600
 # python debiasingModels.py poly-label gb all variance 600
+echo starting4 >> status.txt
+echo $(date) >> status.txt
+python debiasingModels.py poly-label lin coord variance 1000
+echo starting5 >> status.txt
+echo $(date) >> status.txt
